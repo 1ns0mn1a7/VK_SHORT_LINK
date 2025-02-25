4ccd932e904ba412010c41800e040c81ecc86315
1ns0mn1a7 <tyrischev.d.v@gmail.com>
2025-02-25 06:03:04 +0300

    Исправил код

--- main.py
+++ main.py
@@ -17,18 +17,13 @@ def shorten_link(token, url):
         "v": "5.199"
     }
     response = requests.get(api_url, params=params).json()
-
     if "error" in response:
-        raise VKAPIError(
-            f"Ошибка: {response['error']['error_msg']}"
-        )
-
+        raise VKAPIError(f"Ошибка: {response['error']['error_msg']}")
     return response["response"]["short_url"]
 
 
 def count_clicks(token, shortened_url):
-    key = extract_key_from_url(shortened_url)
-
+    key = urlparse(shortened_url).path.lstrip('/')
     api_url = "https://api.vk.com/method/utils.getLinkStats"
     params = {
         "access_token": token,
@@ -37,17 +32,25 @@ def count_clicks(token, shortened_url):
         "extended": "0",
         "v": "5.199"
     }
-
     response = requests.get(api_url, params=params).json()
-
     if "error" in response:
-        raise VKAPIError(
-            f"Ошибка: {response['error']['error_msg']}"
-        )
+        raise VKAPIError(f"Ошибка: {response['error']['error_msg']}")
+    stats = response.get("response", {}).get("stats", [{}])
+    return stats[0].get("views", None)
 
-    return response.get("response", {}).get("stats", [{}])[0].get(
-        "views", None
-    )
+
+def is_shorten_link(token, url):
+    key = urlparse(url).path.lstrip('/')
+    api_url = "https://api.vk.com/method/utils.getLinkStats"
+    params = {
+        "access_token": token,
+        "key": key,
+        "interval": "forever",
+        "extended": "0",
+        "v": "5.199"
+    }
+    response = requests.get(api_url, params=params).json()
+    return "error" not in response
 
 
 def main():
@@ -57,7 +60,7 @@ def main():
     url = input("Введите ссылку: ")
 
     try:
-        if is_short_url(url):
+        if is_shorten_link(token, url):
             clicks = count_clicks(token, url)
             print(
                 "Количество кликов по ссылке:",
@@ -65,18 +68,13 @@ def main():
             )
         else:
             shortened_url = shorten_link(token, url)
-            print("Сокращенная ссылка:", shortened_url)
+            print(
+                "Сокращённая ссылка:",
+                shortened_url
+            )
 
     except VKAPIError as error:
         print(error)
-
-
-def is_short_url(url):
-    return urlparse(url).netloc == "vk.cc"
-
-
-def extract_key_from_url(url):
-    return urlparse(url).path.lstrip('/')
 
 
 if __name__ == "__main__":
